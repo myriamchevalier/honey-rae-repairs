88f2e89064be954530afa1054d66cdec8919aab6
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-08-27 14:40:12 -0500

    added a hook to display specialties

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -4,6 +4,7 @@ export const EmployeeList = () => {
 
 
     const [employees, setEmployees] = useState([])
+    const [specialtiesMessage, displaySpecialties] = useState([])
 
     useEffect(
         () => {
@@ -18,8 +19,20 @@ export const EmployeeList = () => {
         },
         []
     )
+    useEffect(() => {
+            const employeeSpecialty = employees.map(employee => employee.specialty)
+            displaySpecialties(employeeSpecialty.join(", "))
+        }
+    , [employees]
+    
+    )
+
+
     return (
-        <>
+        <> 
+            <div>
+                Specialties: { specialtiesMessage }
+            </div>
             {employees.map(
                 (employeeObject) => {
                     return <p key={`employee--${employeeObject.id}`}>{employeeObject.name}</p>
