61b48d8b4c7edb52725f4f1e5173a2dcf48d7efd
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-09-03 13:59:49 -0500

    added function to save newly assigned employee

--- src/components/serviceTickets/Ticket.js
+++ src/components/serviceTickets/Ticket.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 
 export const Ticket = () => {
     const [ticket, set] = useState({})  // State variable for current ticket object
     const { ticketId } = useParams()  // Variable storing the route parameter
     const [employees, setEmployees] = useState([])
+    const history = useHistory()
 
     useEffect(
         () => {
@@ -29,16 +30,40 @@ export const Ticket = () => {
         []
     )
 
+    const assignEmployee = (changeEvent) => {
+
+        const newTicketObject = {
+        "customerId": parseInt(localStorage.getItem("honey_customer")),
+        "employeeId":parseInt(changeEvent.target.value),
+        "description": ticket.description,
+        "emergency": ticket.emergency,
+        "dateCompleted": ticket.dateCompleted
+        }
+
+        fetch(`http://localhost:8088/serviceTickets/${ticketId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newTicketObject)
+        })
+        .then(
+            () => {
+                history.push("/tickets")
+            }
+        )
+    }
+
     return (
         <>
             <section className="ticket">
                 <h3 className="ticket__description">{ticket.description}</h3>
                 <div className="ticket__customer">Submitted by {ticket.customer?.name}</div>
                 <div className="ticket__employee">Assigned to 
-                    <select id="employee">
+                    <select id="employee" value={ticket.employeeId} onChange={ assignEmployee }>
                         {
                             employees.map((employee) => {
-                                return <option key={`employee--${employee.id}`}>
+                                return <option value={employee.id} key={`employee--${employee.id}`}>
                                     {employee.name}
                                 </option>
                             })
